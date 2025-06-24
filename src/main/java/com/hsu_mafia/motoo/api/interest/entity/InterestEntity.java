4cd1467b909df7404342796c41c91881f46cb301
sanghun <vitash1215@gmail.com>
2025-06-25 00:28:59 +0900

    feat : InterestEntity 설정

---
+++ src/main/java/com/hsu_mafia/motoo/api/interest/entity/InterestEntity.java
@@ -0,0 +1,26 @@
+package com.hsu_mafia.motoo.api.interest.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import com.hsu_mafia.motoo.global.common.BaseEntity;
+
+@Entity
+@Table(name = "interest")
+@NoArgsConstructor
+@Getter
+public class InterestEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String stockId;
+
+    private Long userId;
+
+    private Boolean isInterested;
+} 
