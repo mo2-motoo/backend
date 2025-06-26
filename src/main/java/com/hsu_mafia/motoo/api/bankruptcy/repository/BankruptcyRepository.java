a8b17f26360e1513028bae451edc81cd5072f13a
sanghun <vitash1215@gmail.com>
2025-06-26 18:08:59 +0900

    feat : 파산 신청 관련 로직 작성

--- src/main/java/com/hsu_mafia/motoo/api/bankruptcy/repository/BankruptcyRepository.java
+++ src/main/java/com/hsu_mafia/motoo/api/bankruptcy/repository/BankruptcyRepository.java
@@ -2,6 +2,10 @@ package com.hsu_mafia.motoo.api.bankruptcy.repository;
 
 import com.hsu_mafia.motoo.api.bankruptcy.entity.BankruptcyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.hsu_mafia.motoo.api.user.entity.UserEntity;
+import java.util.List;
 
 public interface BankruptcyRepository extends JpaRepository<BankruptcyEntity, Long> {
+    List<BankruptcyEntity> findAllByUser(UserEntity user);
+    BankruptcyEntity findByUserAndBankruptcyNo(UserEntity user, Integer bankruptcyNo);
 } 
