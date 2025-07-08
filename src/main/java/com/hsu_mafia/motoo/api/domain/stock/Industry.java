12ad186510a06945a9c67b73d5e41255738cf812
sanghun <vitash1215@gmail.com>
2025-07-08 16:08:45 +0900

    feat : 주식 산업군

---
+++ src/main/java/com/hsu_mafia/motoo/api/domain/stock/Industry.java
@@ -0,0 +1,24 @@
+package com.hsu_mafia.motoo.api.domain.stock;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import com.hsu_mafia.motoo.global.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Industry extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @OneToMany(mappedBy = "industry", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Stock> stocks = new ArrayList<>();
+} 
