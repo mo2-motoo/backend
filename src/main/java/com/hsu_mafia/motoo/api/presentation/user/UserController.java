2c640b01e239e5d5f8d5f200f447029040d2bc4b
sanghun <vitash1215@gmail.com>
2025-08-12 13:26:34 +0900

    refactor : api 경로 수정

--- src/main/java/com/hsu_mafia/motoo/api/presentation/user/UserController.java
+++ src/main/java/com/hsu_mafia/motoo/api/presentation/user/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
